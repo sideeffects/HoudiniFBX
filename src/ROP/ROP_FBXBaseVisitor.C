2a49235b81aba1a5953cda746b7064d6d6e19ba4
Damien Pernuit <damienp@sidefx.com>
2018-02-26 17:47:22 -0500

    Updated Copyrights for the ROP_FBX

--- src/ROP/ROP_FBXBaseVisitor.C
+++ src/ROP/ROP_FBXBaseVisitor.C
@@ -2,9 +2,8 @@
  * Copyright (c) 2017
  *	Side Effects Software Inc.  All rights reserved.
  *
- * Redistribution and use of Houdini Development Kit samples in source and
- * binary forms, with or without modification, are permitted provided that the
- * following conditions are met:
+ * Redistribution and use of in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
  *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
